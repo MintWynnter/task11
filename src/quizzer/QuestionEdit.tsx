33371fa3cdbd1ae45bd791e649b098c84665d0e6
Sean <sosull@udel.edu>
2022-10-27 09:56:15 -0400

    lint happy

--- src/quizzer/QuestionEdit.tsx
+++ src/quizzer/QuestionEdit.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import { Question, QuestionType } from "../interfaces/question";
+import { Question } from "../interfaces/question";
 
 import "./QuestionEdit.css";
 
